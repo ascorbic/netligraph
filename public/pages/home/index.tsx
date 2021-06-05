24f80bb6ca241ffba9c403dfbf731bad3616fa66
ascorbic <ascorbic@gmail.com>
2021-06-05 09:48:49 +0100

    Add all the services

--- public/pages/home/index.tsx
+++ public/pages/home/index.tsx
@@ -5,9 +5,9 @@ import OneGraphAuth from "onegraph-auth";
 const APP_ID = "b6a66044-13f0-4049-8d13-6d906de655d8";
 
 export default function Home() {
-  const [loggedIn, setLoggedIn] = useState(false);
   const [token, setToken] = useState("");
   const [auth, setAuth] = useState(undefined);
+  const [servicesStatus, setServicesStatus] = useState({});
 
   useEffect(() => {
     setAuth(
@@ -22,34 +22,88 @@ export default function Home() {
       return;
     }
     setToken(auth.accessToken().accessToken);
-  }, [auth, loggedIn]);
-
-  const doLogin = useCallback(async () => {
-    await auth.login("github");
-    const isLoggedIn = await auth.isLoggedIn("github");
-    setLoggedIn(isLoggedIn);
   }, [auth]);
 
-  useEffect(() => {
+  const refreshServices = useCallback(async () => {
     if (!auth) {
       return;
     }
-    auth.isLoggedIn("github").then(setLoggedIn);
+    const status = await auth.servicesStatus();
+    console.log(status);
+    setServicesStatus(status);
+    setToken(auth.accessToken().accessToken);
   }, [auth]);
+
+  const doLogin = useCallback(
+    async (service) => {
+      await auth.login(service);
+      refreshServices();
+    },
+    [auth, refreshServices]
+  );
+
+  const doLogout = useCallback(
+    async (service) => {
+      await auth.logout(service);
+      refreshServices();
+    },
+    [auth, refreshServices]
+  );
+
+  useEffect(() => {
+    refreshServices();
+  }, [refreshServices]);
+
+  const dataURI = token
+    ? `data:text/plain;charset=utf-8,${encodeURIComponent(
+        `ONEGRAPH_TOKEN=${token}`
+      )}`
+    : null;
 
   return (
     <>
       <section class={styles.home}>
-        <h1>Home</h1>
-        <>
-          {loggedIn ? (
-            <p>Logged in</p>
-          ) : (
-            <button onClick={doLogin}>Login</button>
-          )}
-        </>
-        <input value="ONEGRAPH_TOKEN" readonly />
-        <input value={token} readonly />
+        <table>
+          {auth?.supportedServices.map((service) => {
+            if (service === "zeit") {
+              return;
+            }
+            const loggedIn = servicesStatus?.[service]?.isLoggedIn;
+            return (
+              <tr key={service}>
+                <td>
+                  {loggedIn ? "✅ " : "❌ "}
+                  {auth.friendlyServiceName(service)}
+                </td>
+                <td>
+                  <button
+                    style={{ marginLeft: 20 }}
+                    onClick={() =>
+                      loggedIn ? doLogout(service) : doLogin(service)
+                    }
+                  >
+                    {loggedIn ? "Logout" : "Login"}
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+
+        {token && (
+          <>
+            <input value="ONEGRAPH_TOKEN" readonly />
+            <input value={token} readonly />
+
+            <p>
+              Copy or download the token <em>after</em> logging-in to all
+              required services, as the token changes.{" "}
+              <a href={dataURI} download="netlify.env">
+                Download .env
+              </a>
+            </p>
+          </>
+        )}
       </section>
     </>
   );
